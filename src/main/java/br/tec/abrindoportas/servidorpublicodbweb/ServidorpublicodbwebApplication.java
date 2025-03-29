150afb704e31a8210a209573740e1a87e10472de
Edir P Santos <fennder@gmail.com>
2025-03-29 18:47:33 -0300

    First commit the project

---
+++ src/main/java/br/tec/abrindoportas/servidorpublicodbweb/ServidorpublicodbwebApplication.java
@@ -0,0 +1,13 @@
+package br.tec.abrindoportas.servidorpublicodbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorpublicodbwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorpublicodbwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/tec/abrindoportas/servidorpublicodbweb/ServidorpublicodbwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.abrindoportas.servidorpublicodbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorpublicodbwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
